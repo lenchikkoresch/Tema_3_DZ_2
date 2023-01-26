14deb1021d866521e224355c19cd7ef970a416a2
lenschik.kor <lenschik.kor@yandex.ru>
2023-01-27 01:29:15 +0300

    Тема_3_семенар_2_пример_1_Задание

--- T_3_S_2_Primer_1/Program.cs
+++ T_3_S_2_Primer_1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу. которая выводит случайные цифры из отрезка 
+и показывает наибольшую цифру из числа:
+Например:
+78 -> 8
+12 -> 2
+85 -> 8 */
