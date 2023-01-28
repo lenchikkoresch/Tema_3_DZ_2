0fe46d06226a459c209ce34c2a916dfb36f3dfce
lenschik.kor <lenschik.kor@yandex.ru>
2023-01-28 03:34:28 +0300

    Пример_1.

--- T_3_S_2_Primer_1/Program.cs
+++ T_3_S_2_Primer_1/Program.cs
@@ -4,3 +4,36 @@
 78 -> 8
 12 -> 2
 85 -> 8 */
+
+
+/*
+1. метод, который получает случайное число
+2. метод, который получает максимальную цифру числа
+*/
+
+//метод, который получает случайное число
+int GetRandomNumber(int leftBound, int rightBound)
+{
+int result = 0;
+Random rnd = new Random();
+
+result = rnd.Next(leftBound, rightBound + 1);// [leftBoung, rightBound)
+return result;
+}
+
+//метод, который получает максимальную цифру числа
+int GetMaxDigitOfNumber(int number)
+{
+int firstDigit = number / 10;// 96 / 10 = 9,6 деление нацело на 10, первая цифра
+int secondDigit = number % 10;
+
+if(firstDigit > secondDigit)
+return firstDigit;
+else
+return secondDigit;
+}
+
+int number = GetRandomNumber(10, 99);
+int maxDigit = GetMaxDigitOfNumber(number);
+
+Console.WriteLine($"Максимальная цифра числа {number} = {maxDigit}");
