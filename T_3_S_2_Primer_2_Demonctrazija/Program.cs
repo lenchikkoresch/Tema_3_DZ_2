2677055edb9d02544e9a5cb89b06889e6cfdc7ef
lenschik.kor <lenschik.kor@yandex.ru>
2023-01-28 05:05:28 +0300

    Пример_2_Демонстрация_итог

---
+++ T_3_S_2_Primer_2_Demonctrazija/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+int GetRandomValue()
+{
+return new Random().Next(100, 1000);
+}
+
+int DeleteSecondNumber(int number)
+{
+return (number / 100) * 10 + (number % 10);
+}
+
+int number = GetRandomValue();
+int result = DeleteSecondNumber(number);
+Console.WriteLine($"Было {number}, стало {result}.");
