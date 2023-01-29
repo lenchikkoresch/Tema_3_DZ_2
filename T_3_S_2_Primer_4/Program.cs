5de5bcafdc202f32a072d27be82f3cb1ddf10b1a
lenschik.kor <lenschik.kor@yandex.ru>
2023-01-30 00:27:30 +0300

    Рушуние_пример_4

--- T_3_S_2_Primer_4/Program.cs
+++ T_3_S_2_Primer_4/Program.cs
@@ -7,3 +7,22 @@
 161 -> да
 */
 
+Console.WriteLine("Введите число");
+int GetNumber()
+{string numberStr = Console.ReadLine();
+int a = Convert.ToInt32(numberStr);
+return a;
+}
+
+int firsta = GetNumber();
+
+if (firsta % 7 == 0 && firsta % 23 == 0 )
+{
+Console.WriteLine("Да");
+}
+else
+{
+Console.WriteLine("нет");
+}
+
+
