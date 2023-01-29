f06c4369e81f942f0006fd7e455464ec33049244
lenschik.kor <lenschik.kor@yandex.ru>
2023-01-30 01:33:15 +0300

    Решение_пример_5

--- T_3_S_2_Primer_5/Program.cs
+++ T_3_S_2_Primer_5/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход два числа и проверяет, 
+является ли одно число квадратом другого.
+
+5, 25 -> да
+-4, 16 -> да
+25, 5 -> да
+8,9 -> нет 
+*/
+Console.WriteLine("Введите два числа");
+int GetNumber()
+{
+string numberStr = Console.ReadLine();
+int a = Convert.ToInt32(numberStr);
+return a;
+}
+
+ int a1 = GetNumber();
+ int a2 = GetNumber();
+
+ if (a1 * a1 == a2 || a2 * a2 == a1)
+ {
+    Console.WriteLine("Да");
+ }
+ else
+ {
+    Console.WriteLine("Нет");
+ }
+ 
