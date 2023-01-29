b7156e82810fd156c52e66fbbfed8d0c38e47e9d
lenschik.kor <lenschik.kor@yandex.ru>
2023-01-30 00:05:32 +0300

    Пример_3_решение

--- T_3_S_2_Primer_3/Program.cs
+++ T_3_S_2_Primer_3/Program.cs
@@ -6,3 +6,21 @@
 34, 5 -> не кратно, остаток 4
 16, 4 -> кратно
 */
+int GetNumber()
+{
+int number = Convert.ToInt32(Console.ReadLine());
+return number;
+}
+
+int firstNumber = GetNumber();
+int secondNumber = GetNumber();
+
+if (secondNumber % firstNumber == 0)
+{
+Console.WriteLine($"второе число кратно первому");
+}
+else
+{
+Console.WriteLine(secondNumber % firstNumber);
+}
+
