8bff4772d914b86f374193f7a7efd4ac45f9b3e3
lenschik.kor <lenschik.kor@yandex.ru>
2023-01-30 03:19:43 +0300

    Решение_задачи_10

--- DZ_2/DZ_2_10/Program.cs
+++ DZ_2/DZ_2_10/Program.cs
@@ -5,4 +5,28 @@
 782 -> 8
 918 -> 1
 */
+Console.WriteLine("Введие трехзначное число");
 
+int GetNumber()
+{
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+return number;
+}
+
+int SecondNumber(int number)
+{
+int c = ((number/10)%10);
+return c;
+}
+int number = GetNumber();
+int result = SecondNumber(number);
+
+if (number < 99 || number > 999)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine($"Было {number}, стало {result}.");
+}
