624808987fb3b9c801b40d3d894f33f2889efb5c
lenschik.kor <lenschik.kor@yandex.ru>
2023-01-30 21:55:27 +0300

    Решение_задачи_15

--- DZ_2/DZ_2_15/Program.cs
+++ DZ_2/DZ_2_15/Program.cs
@@ -6,3 +6,35 @@
 7 -> да
 1 -> нет
 */
+Console.WriteLine("Введите число, обозначающее день недели");
+
+int GetNumber()
+{
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+return number;
+}
+
+int SundayNumber (int number)
+{
+    if ((number < 1) || (number > 7))
+{
+    Console.WriteLine("Такого дня недели нет");
+}
+else
+{
+    if ((number == 6) || (number == 7))
+    {
+      Console.WriteLine("Да");
+    }
+    else
+    {
+    Console.WriteLine("Нет");
+    }
+}
+return number; 
+}
+
+int number = GetNumber();
+Console.Write(SundayNumber(number));
+
