604dc4eb85e537273e27db07e8a4fd7e35389faf
lenschik.kor <lenschik.kor@yandex.ru>
2023-01-30 04:30:33 +0300

    Решение_задачи_13

--- DZ_2/DZ_2_13/Program.cs
+++ DZ_2/DZ_2_13/Program.cs
@@ -7,3 +7,36 @@
 
 32679 -> 6
 */
+Console.WriteLine("Введие число");
+
+int GetNumber()
+{
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+return number;
+}
+
+int MakeArray(int a, int b)
+{
+int result = 0;
+    if (b < 3)
+    {
+        Console.Write("Третьей цифры нет");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+return result;
+}
+
+int number = GetNumber();
+int count = number.ToString().Length;
+Console.Write(MakeArray(number, count));
+
